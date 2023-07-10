11a7d4a63ee8c8a4edb9013e490ba50145b15ca0
min <203852295@qq.com>
2023-07-10 16:22:03 +0800

    answer:2-return-type

--- questions/00002-medium-return-type/my-answer.ts
+++ questions/00002-medium-return-type/my-answer.ts
@@ -11,4 +11,20 @@
   }
   type a = MyReturnType<typeof fn> // 应推导出 "1 | 2"
 */
-export type MyReturnType<T extends Function> = typeof T
+// export type MyReturnType<T extends Function> = typeof T
+
+const fn = (v: boolean) => {
+  if (v)
+    return 1
+  else
+    return 2
+}
+
+export type MyReturnType<T extends Function> =
+  T extends (...args: any) => infer R
+    ? R
+    : never
+
+type rt = ReturnType<typeof fn>
+
+type rt2 = MyReturnType<typeof fn>
